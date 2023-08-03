861662f1de422f6225e1d7dc2a18abd220dc9e44
Frankenstein9585 <donny9585@gmail.com>
2023-08-03 17:39:43 +0100

    Done task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+This file contains a function that adds two floats and returns a float
+"""
+
+
+def add(a: float, b: float) -> float:
+    """This function adds two floats and returns a float"""
+    return a + b
