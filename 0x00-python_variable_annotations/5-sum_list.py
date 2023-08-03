26f2221c65db54d81b437b904de13c683d1cb4ff
Frankenstein9585 <donny9585@gmail.com>
2023-08-03 18:37:39 +0100

    Done task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This file contains a function that takes a list of floats
+and returns their sum as float
+"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """This function returns the string representation of a float"""
+    return sum(input_list)
