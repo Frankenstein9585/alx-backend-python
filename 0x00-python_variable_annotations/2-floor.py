d6e2e6a17c863a41cffa6e045df70ffaf61b6de1
Frankenstein9585 <donny9585@gmail.com>
2023-08-03 17:53:28 +0100

    Done task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+This file contains a function that returns the floor of a float
+"""
+
+
+def floor(n: float) -> int:
+    """This function returns the floor of a float"""
+    return int(n)
