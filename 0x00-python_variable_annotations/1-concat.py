e83fce6ac6b8f57d4ca8158e354ccbf9ade7f6a0
Frankenstein9585 <donny9585@gmail.com>
2023-08-03 17:45:43 +0100

    Done task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+This file contains a function that concatenates two strings
+"""
+
+
+def concat(a: str, b: str) -> str:
+    """This function concatenates two strings"""
+    return a + b
