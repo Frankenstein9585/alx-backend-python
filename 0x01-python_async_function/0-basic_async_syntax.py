75751faf64f005fd4eef4737b9511e017ce42fa7
Frankenstein9585 <donny9585@gmail.com>
2023-08-07 22:18:46 +0100

    Done task 0

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""This file contains an asynchronous function that returns a random float"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay: float = 10) -> float:
+    """This function returns a random float"""
+    await asyncio.sleep(max_delay)
+    return random.random() * max_delay
