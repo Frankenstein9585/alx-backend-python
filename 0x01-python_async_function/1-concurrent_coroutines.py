09fd302578fab5ac54641602359f687b04798498
Frankenstein9585 <donny9585@gmail.com>
2023-08-07 23:33:44 +0100

    Done task 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,14 +2,12 @@
 """This file contains an asynchronous function that
 returns a list of random floats"""
 import asyncio
-from typing import Any
+from typing import Any, List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> tuple[Any]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """This function returns a list of random floats"""
-    tasks: list = [wait_random(max_delay) for i in range(n)]
-    delays = await asyncio.gather(*tasks)
-    delays.sort()
-    return delays
+    tasks: list = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    return [await task for task in asyncio.as_completed(tasks)]
