30f9badb8de411af80e981ffcc93dce8fbca6cc5
Frankenstein9585 <donny9585@gmail.com>
2023-08-07 23:35:31 +0100

    Fixed pycodestyle

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -9,5 +9,6 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """This function returns a list of random floats"""
-    tasks: list = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    tasks: list = \
+        [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
     return [await task for task in asyncio.as_completed(tasks)]
