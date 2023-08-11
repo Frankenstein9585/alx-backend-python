124cf8a95a7d999f8be3e4758337aa47fad669fe
Frankenstein9585 <donny9585@gmail.com>
2023-08-11 19:13:07 +0100

    Done task 0

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""This file creates a coroutine called async generator"""
+import asyncio
+import random
+
+
+async def async_generator():
+    """This function generates 10 random numbers
+    waiting one second between each number"""
+    for i in range(10):
+        yield random.uniform(0, 10)
+        await asyncio.sleep(1)
