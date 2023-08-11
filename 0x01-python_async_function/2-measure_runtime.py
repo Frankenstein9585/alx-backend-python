7f53e481770c9e8314e558145e5fd1537c7f083e
Frankenstein9585 <donny9585@gmail.com>
2023-08-11 16:00:24 +0100

    Done task 2

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This file contains an asynchronous function that computes
+the total execution time of the wait_n function
+"""
+import asyncio
+import time
+from typing import List
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """This function computes the time it takes to run
+    an async function"""
+    s = time.perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    return (time.perf_counter() - s) / n
