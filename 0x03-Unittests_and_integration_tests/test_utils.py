838b5cfb9f831748f6bb95bab2b4eeca009a3ae0
Frankenstein9585 <donny9585@gmail.com>
2023-08-27 22:02:40 +0100

    Implemented test_access_nested_map()

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,22 @@
+#!usr/bin/env python3
+"""Test Suite for utils.py"""
+from typing import Mapping, Sequence
+
+from parameterized import parameterized
+
+from utils import access_nested_map
+
+import unittest
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """Tests the access_nested_map method"""
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map: Mapping, path: Sequence, result):
+        """Test Access Nested Map"""
+        self.assertEqual(access_nested_map(nested_map, path), result)
